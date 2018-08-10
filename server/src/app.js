5c31ee26cecdd082b2d51d21c20573890320cc49
karkran <nikhil.karkra@anz.com>
2018-08-11 01:39:00 +1000

    add winston and morgan for logging and use them instead of console.*

--- server/src/app.js
+++ server/src/app.js
@@ -1,13 +1,16 @@
 // npm packages
 import express from 'express';
 import bodyParser from 'body-parser'
+import morgan from 'morgan';
+import {logger} from './util';
+
 // init app
 const app = express();
-
+// setup logging
+app.use(morgan('combined', {stream: logger.stream}));
 // add body parser
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true})); // for parseing application/x-www-form-urlencoded
-
 // test method
 app.get('/', (req, res) => {
   res.send('Hello world!');
@@ -15,7 +18,7 @@ app.get('/', (req, res) => {
 
 // catch all unhandler errors
 app.use((err, req, res) => {
-  console.error(err.stack);
+  logger.error(err.stack);
   res.status(500).send(err);
 });
 // esport app
